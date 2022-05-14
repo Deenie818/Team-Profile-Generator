fd2ac75d3a9258dff9fbba5b034f40110fafb16a
Deenie818 <99102981+Deenie818@users.noreply.github.com>
2022-05-14 12:34:05 -0700

    created employee test

---
+++ tests/Employee.test.js
@@ -0,0 +1,28 @@
+
+const Employee = require ('../lib/employee.js');
+const employee = new Employee('Sam', 1, 'sam@gmail.com');
+
+test('creates an employee object', () => {
+    expect(employee.name).toBe('Sam');
+    expect(employee.id).toBe(1);
+    expect(employee.email).toBe('sam@gmail.com');
+
+});
+
+test('tests to getName() method', () => {
+    expect(employee.name).toBe('Sam');
+})
+
+test('tests to getID() method', () => {
+    expect(employee.id).toBe(1);
+
+})
+
+test('tests to getEmail() method', () => {
+    expect(employee.email).toBe('sam@gmail.com'); 
+})
+
+test('tests to getRole() method', () => {
+    expect(employee.getRole()).toBe('Employee');
+
+});
