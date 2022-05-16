1ce50d00f3d881a89d7c2eda9590aaebc8139965
Deenie818 <99102981+Deenie818@users.noreply.github.com>
2022-05-15 19:39:42 -0700

    fixed header and cards

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -116,18 +116,18 @@ function generateHtml(managers, engineers, interns) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
-    <style>
-    #myHeader {
-        background-color: red;
-        color: black;
-        padding: 40px;
-        text-align:center;
-   }
+//     <style>
+//     #myHeader {
+//         background-color: red;
+//         color: black;
+//         padding: 40px;
+//         text-align:center;
+//    }
    </style>
  </head>
  <body>
 
- <h1 id="myHeader">My Team</h1>
+<h1 id="myHeader">My Team</h1>
 
  <div class="container">
  <div class="row">
